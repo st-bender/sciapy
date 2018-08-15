regress: Bugfix, convert SZA to radians
np.cos needs radians of course.

sciapy/regress/__main__.py
@@ -235,7 +235,7 @@ def main():
 			rad_sun_earth = np.vectorize(_r_sun_earth)(pt, tfmt=args.time_format)
 			pv /= rad_sun_earth**2
 		if pn in args.norm_proxies_SZA:
-			pv *= np.cos(sza_intp(pt))
+			pv *= np.cos(np.radians(sza_intp(pt)))
 		proxy_models.append((pn,
 			ProxyModel(pt, pv,
 				center=pn in args.center_proxies.split(','),