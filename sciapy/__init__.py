12869fcacc6c382a11c614fa3740abc0d28214db
Stefan Bender <stefan.bender@ntnu.no>
2018-08-21 18:04:49 +0200

    Update version number
    Time for a new version.

--- sciapy/__init__.py
+++ sciapy/__init__.py
@@ -3,7 +3,7 @@
 Tools to handle SCIAMACHY/Envisat level 1c spectral data,
 and to process level 2 number density data (only NO for now).
 """
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 try:
 	__SCIAPY_SETUP__
