26fc83faca51bb620f3a6df0431947d77603a62a
Stefan Bender <stefan.bender@ntnu.no>
2019-02-05 17:51:02 +0100

    regress: MSLL and more statistics
    Adds more statistics to the model evaluation at the end. We use the
    trained model to predict the test data, which means we don't `compute()`
    again and cannot use the regular `likelihood()` function but have to
    make up our own predictive probability density.
    From that we calculate the "mean standardized log loss" as described in
    R&W, 2006, section 2.5 (2.34).
    
    Also adds "generalized R^2" scores by comparing the model prediction
    with a prediction using the training data's mean and variance. Includes
    several options for the test variance for now, using the provided test
    errors (squared), using the variance from `predict()`, and using the
    predictive variance plus the provided errors (noisy targets).
    
    To avoid calculating the statistics twice, we only report the statistics
    on the test data set, if the train or test fraction is actually smaller
    than one.

--- sciapy/regress/__main__.py
+++ sciapy/regress/__main__.py
@@ -115,7 +115,14 @@ def _train_test_split(times, data, errs, train_frac,
 			times_test, data_test, errs_test)
 
 
-def _log_statistics(model, times, data, errs, samples, lnp, median=False):
+def _log_prob(resid, var):
+	return -0.5 * (np.log(2 * np.pi * var) + resid**2 / var)
+
+
+def _log_statistics(model, times, data, errs,
+		train_times, train_data, train_errs,
+		samples, lnp,
+		median=False):
 	"""Log the statistics of the model against the provided data
 
 	Logs some statistical information about the model and the
@@ -127,61 +134,95 @@ def _log_statistics(model, times, data, errs, samples, lnp, median=False):
 	mdim = len(model.mean.get_parameter_vector())
 	samples_max_lp = np.max(lnp)
 	if median:
-		sample_pos = np.nanmedian(samples)
+		sample_pos = np.nanmedian(samples, axis=0)
 	else:
 		sample_pos = samples[np.argmax(lnp)]
-	model.compute(times, errs)
 	model.set_parameter_vector(sample_pos)
-	test_loglh = model.log_likelihood(data)
-	test_logpost = model.log_prior() + test_loglh
 	# calculate the GP predicted values and covariance
-	gppred, gpcov = model.predict(data, t=times, return_cov=True)
+	gppred, gpcov = model.predict(train_data, t=times, return_cov=True)
 	# the predictive covariance should include the data variance
-	pcov_gp = gpcov + np.diag(errs)**2
+	gpcov[np.diag_indices_from(gpcov)] += errs**2
 	# residuals
 	resid_mod = model.mean.get_value(times) - data  # GP mean model
 	resid_gp = gppred - data  # GP prediction
-	resid_triv = data.mean() - data  # trivial model
+	resid_triv = np.nanmean(train_data) - data  # trivial model
+	_const = ndat * np.log(2.0 * np.pi)
+	test_logpred = -0.5 * (resid_gp.dot(np.linalg.solve(gpcov, resid_gp))
+			+ np.trace(np.log(gpcov))
+			+ _const)
+	# MSLL
+	var_mod = np.nanvar(resid_mod, ddof=mdim)  # mean model variance
+	var_gp = np.nanvar(resid_gp, ddof=ndim)  # gp model variance
+	var_triv = np.nanvar(train_data, ddof=1)  # trivial model variance
+	logpred_mod = _log_prob(resid_mod, var_mod)
+	logpred_gp = _log_prob(resid_gp, var_gp)
+	logpred_triv = _log_prob(resid_triv, var_triv)
+	logging.info("MSLL mean: %s", np.nanmean(-logpred_mod + logpred_triv))
+	logging.info("MSLL gp: %s", np.nanmean(-logpred_gp + logpred_triv))
+	# predictive variances
+	logpred_mod = _log_prob(resid_mod, var_mod + errs**2)
+	logpred_gp = _log_prob(resid_gp, var_gp + errs**2)
+	logpred_triv = _log_prob(resid_triv, var_triv + errs**2)
+	logging.info("pred MSLL mean: %s", np.nanmean(-logpred_mod + logpred_triv))
+	logging.info("pred MSLL gp: %s", np.nanmean(-logpred_gp + logpred_triv))
 	# cost values
 	cost_mod = np.sum(resid_mod**2)
 	cost_triv = np.sum(resid_triv**2)
 	cost_gp = np.sum(resid_gp**2)
 	# chi^2 (variance corrected costs)
-	chisq_mod_gp = model.solver.dot_solve(resid_mod)
 	chisq_mod_ye = np.sum((resid_mod / errs)**2)
 	chisq_triv = np.sum((resid_triv / errs)**2)
-	chisq_gpcov = resid_mod.dot(np.linalg.solve(pcov_gp, resid_mod))
+	chisq_gpcov = resid_mod.dot(np.linalg.solve(gpcov, resid_mod))
 	# adjust for degrees of freedom
 	cost_gp_dof = cost_gp / (ndat - ndim)
 	cost_mod_dof = cost_mod / (ndat - mdim)
 	cost_triv_dof = cost_triv / (ndat - 1)
 	# reduced chi^2
-	chisq_red_mod_gp = chisq_mod_gp / (ndat - ndim)
 	chisq_red_mod_ye = chisq_mod_ye / (ndat - mdim)
 	chisq_red_triv = chisq_triv / (ndat - 1)
 	chisq_red_gpcov = chisq_gpcov / (ndat - ndim)
+	# "generalized" R^2
+	logp_triv1 = np.sum(_log_prob(resid_triv, errs**2))
+	logp_triv2 = np.sum(_log_prob(resid_triv, var_triv))
+	logp_triv3 = np.sum(_log_prob(resid_triv, var_triv + errs**2))
+	log_lambda1 = test_logpred - logp_triv1
+	log_lambda2 = test_logpred - logp_triv2
+	log_lambda3 = test_logpred - logp_triv3
+	gen_rsq1a = 1 - np.exp(-2 * log_lambda1 / ndat)
+	gen_rsq1b = 1 - np.exp(-2 * log_lambda1 / (ndat - ndim))
+	gen_rsq2a = 1 - np.exp(-2 * log_lambda2 / ndat)
+	gen_rsq2b = 1 - np.exp(-2 * log_lambda2 / (ndat - ndim))
+	gen_rsq3a = 1 - np.exp(-2 * log_lambda3 / ndat)
+	gen_rsq3b = 1 - np.exp(-2 * log_lambda3 / (ndat - ndim))
 	# sent to the logger
 	logging.info("train max logpost: %s", samples_max_lp)
-	logging.info("test log_lh: %s, log_post: %s", test_loglh, test_logpost)
-	logging.debug("1a cost mean model: %s, dof adj: %s", cost_mod, cost_mod_dof)
+	logging.info("test log_pred: %s", test_logpred)
+	logging.info("1a cost mean model: %s, dof adj: %s", cost_mod, cost_mod_dof)
 	logging.debug("1c cost gp predict: %s, dof adj: %s", cost_gp, cost_gp_dof)
 	logging.debug("1b cost triv model: %s, dof adj: %s", cost_triv, cost_triv_dof)
+	logging.info("1d var resid mean model: %s, gp model: %s, triv: %s",
+			var_mod, var_gp, var_triv)
 	logging.info("2a adjR2 mean model: %s, adjR2 gp predict: %s",
 			1 - cost_mod_dof / cost_triv_dof, 1 - cost_gp_dof / cost_triv_dof)
 	logging.info("2b red chi^2 mod: %s / triv: %s = %s",
 			chisq_red_mod_ye, chisq_red_triv, chisq_red_mod_ye / chisq_red_triv)
+	logging.info("2c red chi^2 mod (gp cov): %s / triv: %s = %s",
+			chisq_red_gpcov, chisq_red_triv, chisq_red_gpcov / chisq_red_triv)
 	logging.info("3a stand. red chi^2: %s", chisq_red_gpcov / chisq_red_triv)
 	logging.info("3b 1 - stand. red chi^2: %s",
 			1 - chisq_red_gpcov / chisq_red_triv)
-	logging.info("4 GPchi^2 mod: %s, red chi^2: %s",
-			chisq_mod_gp, chisq_red_mod_gp)
+	logging.info("5a generalized R^2: 1a: %s, 1b: %s",
+			gen_rsq1a, gen_rsq1b)
+	logging.info("5b generalized R^2: 2a: %s, 2b: %s",
+			gen_rsq2a, gen_rsq2b)
+	logging.info("5c generalized R^2: 3a: %s, 3b: %s",
+			gen_rsq3a, gen_rsq3b)
 	try:
 		# celerite
 		logdet = model.solver.log_determinant()
 	except TypeError:
 		# george
 		logdet = model.solver.log_determinant
-	_const = ndat * np.log(2.0 * np.pi)
 	logging.debug("5 logdet: %s, const 2: %s", logdet, _const)
 
 
@@ -549,11 +590,19 @@ def main():
 				show_progress=args.progress,
 				optimized=pre_opt, bounds=bounds, return_logpost=True)
 
-		logging.info("Statistics for the test samples")
-		_log_statistics(gpmodel, no_ys_test, no_dens_test, no_errs_test,
-				samples, lnp)
+		if args.train_fraction < 1. or args.test_fraction < 1.:
+			logging.info("Statistics for the test samples")
+			_log_statistics(gpmodel,
+					no_ys_test, no_dens_test, no_errs_test,
+					no_ys_train, no_dens_train, no_errs_train,
+					samples, lnp,
+			)
 		logging.info("Statistics for all samples")
-		_log_statistics(gpmodel, no_ys, no_dens, no_errs, samples, lnp)
+		_log_statistics(gpmodel,
+				no_ys, no_dens, no_errs,
+				no_ys_train, no_dens_train, no_errs_train,
+				samples, lnp,
+		)
 
 		sampl_percs = np.percentile(samples, [2.5, 50, 97.5], axis=0)
 		if args.plot_corner:
