b988a060e93799960ec34837de7f659ed4478f2f
Stefan Bender <stefan.bender@ntnu.no>
2019-02-20 17:37:38 +0100

    regress: Default proxy loader fix
    Uses our package name instead of "__name__" in `resource_filename()` to
    find the default proxy data files. Using the actual name means that we
    can omit the upwards dots ".." in the file names but still need the full
    path name.

--- sciapy/regress/load_data.py
+++ sciapy/regress/load_data.py
@@ -73,18 +73,18 @@ _SPEs = [pd.date_range("2002-04-20", "2002-05-01"),
 
 
 def load_dailymeanAE(
-		filename="../data/indices/AE_Kyoto_1980-2016_daily2_shift12h.dat",
+		filename="data/indices/AE_Kyoto_1980-2016_daily2_shift12h.dat",
 		name="AE", tfmt="jyear"):
 	from pkg_resources import resource_filename
-	AEfilename = resource_filename(__name__, filename)
+	AEfilename = resource_filename("sciapy", filename)
 	return load_solar_gm_table(AEfilename, cols=[0, 1],
 			names=["time", name], tfmt=tfmt)
 
 
-def load_dailymeanLya(filename="../data/indices/lisird_lya3_1980-2017.dat",
+def load_dailymeanLya(filename="data/indices/lisird_lya3_1980-2017.dat",
 		name="Lya", tfmt="jyear"):
 	from pkg_resources import resource_filename
-	Lyafilename = resource_filename(__name__, filename)
+	Lyafilename = resource_filename("sciapy", filename)
 	return load_solar_gm_table(Lyafilename, cols=[0, 1],
 			names=["time", name], tfmt=tfmt)
 
