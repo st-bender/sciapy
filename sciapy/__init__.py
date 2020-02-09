8c9298ca6163956121ed62083077601c3bd8ee62
Stefan Bender <stefan.bender@ntnu.no>
2020-02-09 17:38:28 +0100

    Increase version number
    Time for a little version number bump.

--- sciapy/__init__.py
+++ sciapy/__init__.py
@@ -10,7 +10,7 @@
 Tools to handle SCIAMACHY/Envisat level 1c spectral data,
 and to process level 2 number density data (only NO for now).
 """
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 try:
 	__SCIAPY_SETUP__
