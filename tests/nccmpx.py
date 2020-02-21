bdae2efe2d36faa4e2c26b5c7153e326972d3d68
Stefan Bender <stefan.bender@ntnu.no>
2020-02-21 23:40:05 +0100

    lvl2: Post processing integration tests
    Tests the level-2 post processing chain against the prepared netcdf
    files to help in development and testing. Includes a small script that
    compares netcdfs on some different levels to ensure compatibility with
    previous and future versions.

---
+++ tests/nccmpx.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8
+#
+# Copyright (c) 2020 Stefan Bender
+#
+# This module is part of sciapy.
+# sciapy is free software: you can redistribute it or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, version 2.
+# See accompanying LICENSE file or http://www.gnu.org/licenses/gpl-2.0.html.
+"""Compare netcdf files
+
+Compare netcdf files, testing the variable attributes and values
+to ensure format compatibility.
+"""
+import sys
+import xarray as xr
+
+__all__ = ["nccmpattrs", "ncallclose", "ncequal", "ncidentical"]
+
+
+def nccmpattrs(file1, file2, ignore=[]):
+	"""Compare variable attributes and global attributes"""
+	msg = ""
+	same = True
+	with xr.open_dataset(file1) as ds1:
+		ds2 = xr.open_dataset(file2)
+		for v in ds1.variables:
+			if ds1[v].attrs != ds2[v].attrs:
+				msg = "{0}\nL\t{1}\t{2}\nR\t{1}\t{3}".format(
+					msg, v, ds1[v].attrs, ds2[v].attrs,
+				)
+				same = False
+		for attr in ds1.attrs:
+			lattr = getattr(ds1, attr)
+			rattr = getattr(ds2, attr)
+			if lattr != rattr and attr not in ignore:
+				msg = "{0}\nL\t{1}\t{2}\nR\t{1}\t{3}".format(
+					msg, attr, lattr, rattr,
+				)
+				same = False
+	assert same, msg
+
+
+def ncallclose(file1, file2):
+	with xr.open_dataset(file1) as ds1:
+		ds2 = xr.open_dataset(file2)
+		xr.testing.assert_allclose(ds1, ds2)
+
+
+def ncequal(file1, file2):
+	with xr.open_dataset(file1) as ds1:
+		ds2 = xr.open_dataset(file2)
+		xr.testing.assert_equal(ds1, ds2)
+
+
+def ncidentical(file1, file2, ignore=[]):
+	with xr.open_dataset(file1) as ds1:
+		ds2 = xr.open_dataset(file2)
+		for ign in ignore:
+			del ds1.attrs[ign]
+			del ds2.attrs[ign]
+		xr.testing.assert_identical(ds1, ds2)
+
+
+if __name__ == "__main__":
+	nccmpattrs(*sys.argv[1:])
+	ncallclose(*sys.argv[1:])
+	ncequal(*sys.argv[1:])
+	ncidentical(*sys.argv[1:])
---
+++ tests/test_level2_pp_cli.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8
+#
+# Copyright (c) 2019 Stefan Bender
+#
+# This module is part of sciapy.
+# sciapy is free software: you can redistribute it or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, version 2.
+# See accompanying LICENSE file or http://www.gnu.org/licenses/gpl-2.0.html.
+"""SCIAMACHY level 2 post processing command line interface tests
+
+Test functions to assure that the command line interface works in
+most of the cases.
+"""
+import os
+from subprocess import Popen
+
+from nccmpx import (ncallclose, nccmpattrs, ncequal, ncidentical)
+
+DATADIR = os.path.join(".", "tests", "data")
+IFILE1 = os.path.join(DATADIR, "test_v2.2.nc")
+IFILE2 = os.path.join(DATADIR, "test_v2.2x.nc")
+
+
+def test_pp_help():
+	p = Popen(["scia_post_process_l2.py", "-h"])
+	p.communicate()
+	p.wait()
+	assert p.returncode == 0
+
+
+def test_pp_netcdf(tmpdir):
+	ofile = os.path.join(tmpdir, "test_v2.2_t.nc")
+	p = Popen([
+		"scia_post_process_l2.py",
+		"-A", "The Dude",
+		"-M", "2010-02",
+		"-p", os.path.join(DATADIR, "l2"),
+		"-s", os.path.join(DATADIR, "l1c"),
+		"--mlt",
+		ofile,
+	])
+	p.communicate()
+	p.wait()
+	assert p.returncode == 0
+	ncallclose(IFILE1, ofile)
+	nccmpattrs(IFILE1, ofile, ignore=["creation_time"])
+	ncequal(IFILE1, ofile)
+	ncidentical(IFILE1, ofile, ignore=["creation_time"])
+
+
+def test_pp_xarray(tmpdir):
+	ofile = os.path.join(tmpdir, "test_v2.2x_t.nc")
+	p = Popen([
+		"scia_post_process_l2.py",
+		"-A", "The Dude",
+		"-M", "2010-02",
+		"-p", os.path.join(DATADIR, "l2"),
+		"-s", os.path.join(DATADIR, "l1c"),
+		"--mlt",
+		"-X",
+		ofile,
+	])
+	p.communicate()
+	p.wait()
+	assert p.returncode == 0
+	ncallclose(IFILE2, ofile)
+	nccmpattrs(IFILE2, ofile, ignore=["creation_time"])
+	ncequal(IFILE2, ofile)
+	ncidentical(IFILE2, ofile, ignore=["creation_time"])
