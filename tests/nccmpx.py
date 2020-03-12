7c7a7f7bcd74d13de96615e7dac8acf078c74066
Stefan Bender <stefan.bender@ntnu.no>
2020-03-12 12:02:47 +0100

    tests: Fix netcdf comparison
    Uses `numpy.testing` for the attributes as some of the variables can be
    floats (or nans) and testing for equality can give false negatives.
    `numpy.testing.assert_equal()` treats nans as equal.
    
    Fixes overwriting the state of `same` when comparing the individual
    variable attributes.

--- tests/nccmpx.py
+++ tests/nccmpx.py
@@ -14,6 +14,7 @@ Compare netcdf files, testing the variable attributes and values
 to ensure format compatibility.
 """
 import sys
+import numpy as np
 import xarray as xr
 
 __all__ = ["nccmpattrs", "ncallclose", "ncequal", "ncidentical"]
@@ -26,7 +27,9 @@ def cmpvarattrs(v1, v2):
 	for a in v1.attrs:
 		a1 = getattr(v1, a, None)
 		a2 = getattr(v2, a, None)
-		if a2 is None or a2 != a1:
+		try:
+			np.testing.assert_equal(a1, a2)
+		except AssertionError:
 			msg = "{0}\nL\t{1}\t{2}\t{3}\nR\t{1}\t{2}\t{4}".format(
 				msg, v1.name, a, a1, a2,
 			)
@@ -41,13 +44,16 @@ def nccmpattrs(file1, file2, ignore=[]):
 	with xr.open_dataset(file1, decode_cf=False) as ds1:
 		ds2 = xr.open_dataset(file2, decode_cf=False)
 		for v in ds1.variables:
-			same, vmsg = cmpvarattrs(ds1[v], ds2[v])
-			if not same:
+			vsame, vmsg = cmpvarattrs(ds1[v], ds2[v])
+			if not vsame:
 				msg = "{0}{1}".format(msg, vmsg)
-		for attr in ds1.attrs:
+				same = False
+		for attr in set(ds1.attrs).difference(set(ignore)):
 			lattr = getattr(ds1, attr, None)
 			rattr = getattr(ds2, attr, None)
-			if lattr != rattr and attr not in ignore:
+			try:
+				np.testing.assert_equal(lattr, rattr)
+			except AssertionError:
 				msg = "{0}\nL\t{1}\t{2}\nR\t{1}\t{3}".format(
 					msg, attr, lattr, rattr,
 				)
