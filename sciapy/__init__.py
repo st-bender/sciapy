27f740eb7e4353f6cfb535004b658de9bafdc9ef
Stefan Bender <stefan.bender@ntnu.no>
2022-04-05 22:04:55 +0200

    Update to .dev version
    Indicate new development cycle.
    
    [skip ci]

--- sciapy/__init__.py
+++ sciapy/__init__.py
@@ -10,7 +10,7 @@
 Tools to handle SCIAMACHY/Envisat level 1c spectral data,
 and to process level 2 number density data (only NO for now).
 """
-__version__ = "0.0.7"
+__version__ = "0.0.8.dev0"
 
 from . import level1c
 from . import level2
